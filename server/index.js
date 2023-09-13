acbad11566a3e2bec9dc2e3bd58524c2c3dd3de5
Egor Shkaidurov <private.shkaidurov@gmail.com>
2023-09-13 11:18:10 +0400

    Server updates
    Убрал express, сейчас сервер на голом nodejs

--- server/index.js
+++ server/index.js
@@ -1,8 +1,34 @@
-const express = require('express');
-const app = express();
+const http = require('http');
 
-app.post('/checkUser', function (req, res) {
-    res.send(req)
-})
+http.createServer(function (request, response) {
+  if (request.url === "/auth") {
+    console.log("Server auth...");
+    let data = "";
+        request.on("data", chunk => {
+            data += chunk;
+        });
+        request.on("end", () => {
+            console.log(JSON.parse(data));
+            response.end("Данные успешно получены");
+        });
+  } 
   
-app.listen(7171);
+  else if (request.url === "/regiser") {
+    console.log("Register...");
+    let data = "";
+        request.on("data", chunk => {
+            data += chunk;
+        });
+        request.on("end", () => {
+            console.log(JSON.parse(data));
+            response.end("Данные успешно получены");
+        });
+  } 
+  
+  else {
+    response.statusCode = 404;
+    response.write("Not Found");
+  }
+
+  response.end();
+}).listen(7171);
